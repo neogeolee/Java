5a8f3c4f42cfaa499f837b52e857d48968805c19
neogeolee <neogeolee@nate.com>
2020-05-27 11:11:22 +0900

    Ch01 프로젝트등록

---
+++ Ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,13 @@
+package sub1;
+/*
+ * 날짜 : 2020/04/29
+ * 이름 : 이태훈
+ * 내용 : 자바 HelloWorld 출력하기
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
+	}
+
+}
