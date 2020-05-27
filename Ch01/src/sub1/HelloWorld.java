2a844c6bbb243016f82cdde7e5732409f768522e
neogeolee <neogeolee@nate.com>
2020-05-27 12:10:43 +0900

    Ch01 다시수정

--- Ch01/src/sub1/HelloWorld.java
+++ Ch01/src/sub1/HelloWorld.java
@@ -8,6 +8,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello Java!");
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
 	}
 
 }
