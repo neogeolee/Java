b997de2daa07f6649b97d4b5c8bbb6e7f121f727
neogeolee <neogeolee@nate.com>
2020-05-27 12:43:59 +0900

    ch01

--- Ch01/src/sub1/HelloWorld.java
+++ Ch01/src/sub1/HelloWorld.java
@@ -6,7 +6,7 @@ package sub1;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!~");
 		System.out.println("Hello Java!");
 		System.out.println("Hello World!");
 		System.out.println("Hello Java!");
